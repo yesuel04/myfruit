3bb535f53c9e373561d50f5c5d4f4cc97e1a2438
yesuel <adt04@naver.com>
2025-01-17 12:29:30 +0900

    상세보기 예외처리 및 목록보기 구현

--- src/main/java/com/myfruit/pms/controller/ItemController.java
+++ src/main/java/com/myfruit/pms/controller/ItemController.java
@@ -5,7 +5,10 @@ import com.myfruit.pms.mapper.ItemMapper;
 import com.myfruit.pms.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/items")
@@ -32,19 +35,37 @@ public class ItemController {
 
     @GetMapping("/{id}")
     @ResponseBody
-    public ItemDto getItem(@PathVariable("id") int id){
-        return itemService.getItem(id);
+    public String getItem(@PathVariable("id") int id, Model model){
+        try{
+            ItemDto itemDto = itemService.getItem(id);
+            model.addAttribute("item", itemDto);
+        } catch (IllegalStateException e) {
+            model.addAttribute("message", e.getMessage());
+            return "/common/error/404";
+        }
+        return "shop/detail";
     }
+
+    @GetMapping
+    public String getItemList(Model model){
+        List<ItemDto> items = itemService.getItemList();
+        model.addAttribute("items", items);
+        return "shop/list";
+    }
+
+
 
     // 생성 페이지 GET /items/create
     // 생성 POST /items
     // 상세보기 GET /items/{id}
     // 수정 POST /items/{id}/modify
     // 삭제 GET /items/{id}/delete
+    // 목록 GET /items
 
     // 생성 페이지 GET /items/create
     // 생성 POST /items
     // 상세보기 GET /items/{id}
     // 수정 PUT /items/{id}/modify
     // 삭제 DELETE /items/{id}/delete
+    // 목록 GET /items
 }
