edf8bb2ff2498d9c187e3bd163320612ada5d407
yesuel <adt04@naver.com>
2025-01-17 11:44:09 +0900

    요청주소 설계 - 컨트롤러

---
+++ src/main/java/com/myfruit/pms/controller/ItemController.java
@@ -0,0 +1,50 @@
+package com.myfruit.pms.controller;
+
+import com.myfruit.pms.dto.ItemDto;
+import com.myfruit.pms.mapper.ItemMapper;
+import com.myfruit.pms.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/items")
+public class ItemController {
+
+    @Autowired
+    private ItemMapper itemMapper;
+
+    @Autowired
+    private ItemService itemService;
+
+    @GetMapping("/create")
+    public String create(){
+        return "/shop/create-item";
+    }
+
+    @PostMapping
+    @ResponseBody
+    // item = "애플"
+    public void createItem(@RequestBody ItemDto itemDto){
+        System.out.println(itemDto.getItem());
+        itemService.createItem(itemDto);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ItemDto getItem(@PathVariable("id") int id){
+        return itemService.getItem(id);
+    }
+
+    // 생성 페이지 GET /items/create
+    // 생성 POST /items
+    // 상세보기 GET /items/{id}
+    // 수정 POST /items/{id}/modify
+    // 삭제 GET /items/{id}/delete
+
+    // 생성 페이지 GET /items/create
+    // 생성 POST /items
+    // 상세보기 GET /items/{id}
+    // 수정 PUT /items/{id}/modify
+    // 삭제 DELETE /items/{id}/delete
+}
