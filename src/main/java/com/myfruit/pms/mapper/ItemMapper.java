fa6503ad2b1b4e3f59158787d502a2962dac67e3
yesuel <adt04@naver.com>
2025-01-17 11:43:47 +0900

    서비스 구현

--- src/main/java/com/myfruit/pms/mapper/ItemMapper.java
+++ src/main/java/com/myfruit/pms/mapper/ItemMapper.java
@@ -3,9 +3,26 @@ package com.myfruit.pms.mapper;
 import com.myfruit.pms.dto.ItemDto;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+import java.util.Optional;
+
 @Mapper
 public interface ItemMapper {
-//    ItemDto itemDto = new ItemDto();
-//    itemDto.setItem('딸기');
-    void insertMapper(ItemDto itemDto);
+    //인터페이스 라서 메서드 이름만 있음.
+
+    //insert 하기
+    void insertItem(ItemDto itemDto);
+
+    //하나만 select
+    Optional<ItemDto> getItemById(int id);
+
+    //여러개 List 를 select 하기
+    List<ItemDto> getItemList();
+
+    //update 하기
+    void updateItem(ItemDto itemDto);
+
+    //delete 하기
+    void deleteItem(int id);
+
 }
---
+++ src/main/java/com/myfruit/pms/service/ItemService.java
@@ -0,0 +1,47 @@
+package com.myfruit.pms.service;
+
+import com.myfruit.pms.dto.ItemDto;
+import com.myfruit.pms.mapper.ItemMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ItemService {
+
+    @Autowired
+    private ItemMapper itemMapper;
+
+    //메서드 구문
+    //접근제어자 리턴타입 메서드 이름(매개 변수) {}
+
+    public void createItem(ItemDto itemDto){
+        itemMapper.insertItem(itemDto);
+    }
+
+    public ItemDto getItem(int id) {
+//        Optional<ItemDto> item = itemMapper.getItemById(id);
+//        item.orElseThrow();
+//        return item;
+        return itemMapper.getItemById(id).orElseThrow(
+                () -> new IllegalArgumentException("파일을 찾을 수 없습니다.")
+        );
+    }
+
+    public List<ItemDto> getItemList(){
+        return itemMapper.getItemList();
+    }
+
+    public void modify(ItemDto itemDto){
+        itemMapper.updateItem(itemDto);
+    }
+
+    public void remove(int id){
+        itemMapper.deleteItem(id);
+    }
+
+
+
+}
